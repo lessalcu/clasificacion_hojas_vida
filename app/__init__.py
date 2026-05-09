68438099da6400023240c27e0aaf292b1004855e
Meybili <meybili200219@gmail.com>
2026-05-09 17:45:43 -0500

    Agrega endpoints de versionamiento de modelos

--- app/__init__.py
+++ app/__init__.py
@@ -12,6 +12,7 @@ from app.api.job_profile_routes import job_profile_bp
 from app.api.model_training_routes import model_training_bp
 from app.api.model_validation_routes import model_validation_bp
 from app.api.pdf_extraction_routes import pdf_extraction_bp
+from app.api.model_version_routes import model_version_bp
 from app.config import config_by_name
 from app.errors.handlers import register_error_handlers
 from app.utils.logger import configure_logging
@@ -38,6 +39,7 @@ def create_app():
     app.register_blueprint(dataset_sample_bp)
     app.register_blueprint(model_training_bp)
     app.register_blueprint(model_validation_bp)
+    app.register_blueprint(model_version_bp)
 
     register_error_handlers(app)
 
